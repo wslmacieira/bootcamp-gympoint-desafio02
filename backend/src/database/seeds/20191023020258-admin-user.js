046d02f88cc766f49c45324e08ccb9f54a1538f9
wslmacieira <wslmacieira@outlook.com>
2019-10-22 23:05:21 -0300

    criado seed admin-user

---
+++ backend/src/database/seeds/20191023020258-admin-user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: QueryInterface => {
+    return QueryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Administrador',
+          email: 'admin@gympoint.com',
+          password_hash: bcrypt.hashSync('123456', 8),
+          admin: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: () => {},
+};
