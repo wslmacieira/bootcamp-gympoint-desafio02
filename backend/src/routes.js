e37b6122e0dc82b09c6cf6667a445afd4fc1d403
wslmacieira <wslmacieira@outlook.com>
2019-10-22 22:27:06 -0300

    criando o loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Wagner dos Santos',
+    email: 'wslmacieira@outlook.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
