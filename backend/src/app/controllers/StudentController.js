091025c69323d80e9d2a4acf63f8040a0aa9f883
wslmacieira <wslmacieira@outlook.com>
2019-10-23 15:25:44 -0300

    criado model Student - StudentController e rotas para create/update de student

---
+++ backend/src/app/controllers/StudentController.js
@@ -0,0 +1,53 @@
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const { email } = req.body;
+
+    const student = await Student.findByPk(req.params.id);
+
+    if (email !== student.email) {
+      const studentExists = await Student.findOne({ where: { email } });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists.' });
+      }
+    }
+
+    const { id, name, age, weight, height } = await student.update(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -10,8 +11,12 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// rotas so podem ser acessadas com autenticação por jwt
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
