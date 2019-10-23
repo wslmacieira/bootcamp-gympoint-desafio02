ee8b20cf89e1376dd1c9faf9b27f0f24ff536f0c
wslmacieira <wslmacieira@outlook.com>
2019-10-23 16:41:01 -0300

    validando dados de entrada com a biblioteca Yup

--- backend/src/app/controllers/StudentController.js
+++ backend/src/app/controllers/StudentController.js
@@ -1,7 +1,23 @@
+import * as Yup from 'yup';
+
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
@@ -25,6 +41,20 @@ class StudentController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email } = req.body;
 
     const student = await Student.findByPk(req.params.id);
@@ -37,7 +67,7 @@ class StudentController {
       }
     }
 
-    const { id, name, age, weight, height } = await student.update(req.body)
+    const { id, name, age, weight, height } = await student.update(req.body);
 
     return res.json({
       id,
